65e30a9c22101771ee1acd8e8177209c0f1781ef
sudheergiri <102008317+sudheergiri@users.noreply.github.com>
2022-04-12 16:20:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello from react</h1>
+      <h1>hello from reactapp</h1>
       <h2>This is a jenkins triggered build</h2>
     </div>
   );
