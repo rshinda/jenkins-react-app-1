3608332134eedaf127b93711ee53483f9dc0856b
sudheergiri <102008317+sudheergiri@users.noreply.github.com>
2022-04-13 18:31:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello welcome to sudheer</h1>
+      <h1>hello welcome to ongraph</h1>
       <h2>This is a jenkins triggered build</h2>
     </div>
   );
