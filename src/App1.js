ebd6555a55129d80e34a763422fe7e2a13397adc
sudheergiri <102008317+sudheergiri@users.noreply.github.com>
2022-04-21 17:35:20 +0530

    Create App1.js

---
+++ src/App1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello ongraph</h1>
+      <h2>This is a jenkins triggered build</h2>
+    </div>
+  );
+}
+
+export default App;
