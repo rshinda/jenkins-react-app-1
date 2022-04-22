c6ececafbbf28f6ace21840aef8ae72c960112e6
sudheergiri <102008317+sudheergiri@users.noreply.github.com>
2022-04-22 16:33:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>welcome to react</h1>
-      <h2>This is a jenkins triggered build</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
-
-export default App;
