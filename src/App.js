4ae177ecef5562bb942751c729c50bdb58be8895
sudheergiri <102008317+sudheergiri@users.noreply.github.com>
2022-04-22 17:04:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ export default class App extends React.Component {
     return (
       <div className="component-app">
         <Display value={this.state.next || this.state.total || "0"} />
-        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
